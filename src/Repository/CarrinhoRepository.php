910ea910b0f4bc0dd5b392723b312124b0d2981b
Gabriel Cirqueira <gabrielcirqueira711@gmail.com>
2024-11-25 07:13:39 -0300

    Revidos os comentarios

--- src/Repository/CarrinhoRepository.php
+++ src/Repository/CarrinhoRepository.php
@@ -16,31 +16,6 @@ class CarrinhoRepository extends ServiceEntityRepository
         parent::__construct($registry, Carrinho::class);
     }
 
-    //    /**
-    //     * @return Carrinho[] Returns an array of Carrinho objects
-    //     */
-    //    public function findByExampleField($value): array
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->orderBy('c.id', 'ASC')
-    //            ->setMaxResults(10)
-    //            ->getQuery()
-    //            ->getResult()
-    //        ;
-    //    }
-
-    //    public function findOneBySomeField($value): ?Carrinho
-    //    {
-    //        return $this->createQueryBuilder('c')
-    //            ->andWhere('c.exampleField = :val')
-    //            ->setParameter('val', $value)
-    //            ->getQuery()
-    //            ->getOneOrNullResult()
-    //        ;
-    //    }
-
     public function salvar(Carrinho $carrinho) : void
     {
         $this->getEntityManager()->persist($carrinho);
