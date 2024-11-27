645e1b062d5c9554650cef22d4f1e63e03cbcb7a
OLD-gabriel <gabrielcirqueira711@gmail.com>
2024-11-26 21:46:40 -0300

    ajuste na hora de selecionar clientes e produtos

--- src/Entity/Carrinho.php
+++ src/Entity/Carrinho.php
@@ -43,7 +43,7 @@ class Carrinho
     #[ORM\ManyToMany(targetEntity: Item::class, mappedBy: 'carrinhos')]
     private Collection $items;
 
-    public function __construct(Cliente $cliente,User $usuario,string $status = "Aguardando pagamento",int $valorTotal = null)
+    public function __construct(Cliente $cliente,User $usuario,string $status = "Aguardando pagamento",int $valorTotal = 0)
     {
         $this->cliente = $cliente;
         $this->usuario = $usuario;
